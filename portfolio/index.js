e6dd9d9ac389f48d61d443fd73117a4c377ca6ae
Kirill Kuleuski <kirilllavrov798@gmail.com>
2022-01-21 17:59:52 +0300

    new

--- portfolio/index.js
+++ portfolio/index.js
@@ -16,3 +16,23 @@ for (let anchor of anchors) {
 
 
 console.log ("Моя оценка 100-110 баллов \n 1) Вёрстка валидная +10 \n 2) Вёрстка семантическая +20 \n 3) Вёрстка соответствует макету +48 \n 4) Требования к css выполнены + 12 \n 5) Интерактивность, реализована через css +20 " )
+
+
+// fo Burger
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
