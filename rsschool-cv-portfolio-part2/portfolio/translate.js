156c643067a57d5775d235adce1bc39a593c7b6c
Kirill Kuleuski <kirilllavrov798@gmail.com>
2022-01-28 18:17:18 +0300

    well done

---
+++ rsschool-cv-portfolio-part2/portfolio/translate.js
@@ -0,0 +1,234 @@
+
+const i18Obj = {
+
+  "lang-tittle" : {
+    "ru" : "Портфолио",
+    "en" : "Portfolio",
+  },
+
+  "skills" : {
+    "ru" : "Навыки",
+    "en" : "Skills",
+  },
+
+  "portfolio" : {
+    "ru" : "Портфолио",
+    "en" : "Portfolio",
+  },
+
+  "video" : {
+    "ru" : "Видео",
+    "en" : "Video",
+  },
+
+  "price" : {
+    "ru" : "Цены",
+    "en" : "Price",
+  },
+
+  "contacts" : {
+    "ru" : "Контакты",
+    "en" : "Contacts",
+  },
+
+  "hero-title" : {
+    "ru" : "Алекса Райс",
+    "en" : "Alexa Rise",
+  },
+
+  "hero-text" : {
+    "ru" : "Сохраните искренние эмоции, романтические переживания и счастливые моменты жизни вместе с профессиональным фотографом Алексой Райс",
+    "en" : "Save sincere emotions, romantic feelings and happy moments of life together with professional photographer Alexa Rise",
+  },
+
+  "hire" : {
+    "ru" : "Пригласить",
+    "en" : "Hire me",
+  },
+
+  "skill-title-1" : {
+    "ru" : "Фотография",
+    "en" : "Digital photography",
+  },
+
+  "skill-text-1" : {
+    "ru" : "Высококачественные фото в студии и на природе",
+    "en" : "High-quality photos in the studio and on the nature",
+  },
+
+  "skill-title-2" : {
+    "ru" : "Видеосъемка",
+    "en" : "Video shooting",
+  },
+
+  "skill-text-2" : {
+    "ru" : "Запечатлите лучшие моменты, чтобы они всегда оставались с вами",
+    "en" : "Capture your moments so that they always stay with you",
+  },
+
+  "skill-title-3" : {
+    "ru" : "Ретушь",
+    "en" : "Rotouch",
+  },
+
+  "skill-text-3" : {
+    "ru" : "Я стремлюсь к тому, чтобы фотография превосходила реальность",
+    "en" : "I strive to make photography surpass reality",
+  },
+
+  "skill-title-4" : {
+    "ru" : "Звук",
+    "en" : "Audio",
+  },
+
+  "skill-text-4" : {
+    "ru" : "Профессиональная запись звука для видео, рекламы, портфолио",
+    "en" : "Professional sounds recording for video, advertising, portfolio",
+  },
+
+  "winter" : {
+    "ru" : "Зима",
+    "en" : "Winter",
+  },
+
+  "spring" : {
+    "ru" : "Весна",
+    "en" : "Spring",
+  },
+
+  "summer" : {
+    "ru" : "Лето",
+    "en" : "Summer",
+  },
+
+  "autumn" : {
+    "ru" : "Осень",
+    "en" : "Autumn",
+  },
+
+  "price-header-1" : {
+    "ru" : "Стандарт",
+    "en" : "Standard",
+  },
+
+  "price-header-2" : {
+    "ru" : "Премиум",
+    "en" : "Premium",
+  },
+
+  "price-header-3" : {
+    "ru" : "Голд",
+    "en" : "Gold",
+  },
+  
+  "price-description-1-span-1" : {
+    "ru" : "Одна локация",
+    "en" : "One location",
+  },
+
+  "price-description-1-span-2" : {
+    "ru" : "120 цветных фото",
+    "en" : "120 photos in color",
+  },
+
+  "price-description-1-span-3" : {
+    "ru" : "12 отретушированных фото",
+    "en" : "12 photos in retouch",
+  },
+
+  "price-description-1-span-4" : {
+    "ru" : "Готовность через 2-3 недели",
+    "en" : "Readiness 2-3 weeks",
+  },
+
+  "price-description-1-span-5" : {
+    "ru" : "Макияж, визаж",
+    "en" : "Make up, visage",
+  },
+
+  "price-description-2-span-1" : {
+    "ru" : "Одна-две локации",
+    "en" : "One or two locations",
+  },
+
+  "price-description-2-span-2" : {
+    "ru" : "200 цветных фото",
+    "en" : "200 photos in color",
+  },
+
+  "price-description-2-span-3" : {
+    "ru" : "20 отретушированных фото",
+    "en" : "200 photos in color",
+  },
+
+  "price-description-2-span-4" : {
+    "ru" : "Готовность через 1-2 недели",
+    "en" : "Readiness 1-2 weeks",
+  },
+
+  "price-description-2-span-5" : {
+    "ru" : "Макияж, визаж",
+    "en" : "Make up, visage",
+  },
+
+  "price-description-3-span-1" : {
+    "ru" : "Три локации и больше",
+    "en" : "Three locations or more",
+  },  
+
+  "price-description-3-span-2" : {
+    "ru" : "300 цветных фото",
+    "en" : "300 photos in color",
+  },  
+
+  "price-description-3-span-3" : {
+    "ru" : "50 отретушированных фото",
+    "en" : "50 photos in retouch",
+  },  
+
+  "price-description-3-span-4" : {
+    "ru" : "Готовность через 1 неделю",
+    "en" : "Readiness 1 week",
+  },  
+
+  "price-description-3-span-5" : {
+    "ru" : "Макияж, визаж, прическа",
+    "en" : "Make up, visage, hairstyle",
+  },  
+
+  "order" : {
+    "ru" : "Заказать съемку",
+    "en" : "Order shooting",
+  },  
+
+  "contact-me" : {
+    "ru" : "Свяжитесь со мной",
+    "en" : "Contact me",
+  },  
+
+  "send-message" : {
+    "ru" : "Отправить сообщение",
+    "en" : "Send message",
+  },  
+
+  "email" : {
+    "ru" : "Емейл",
+    "en" : "E-mail",
+  },  
+
+  // "email" : {
+  //   "ru" : "Емейл",
+  //   "en" : "E-mail",
+  // },  
+
+  // "tel" : {
+  //   "ru" : "Телефон",
+  //   "en" : "Phone",
+  // },  
+
+  // "message" : {
+  //   "ru" : "Сообщение",
+  //   "en" : "Message",
+  // },  
+  }
+  // export default i18Obj;
